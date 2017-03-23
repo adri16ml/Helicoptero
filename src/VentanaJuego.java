9997c12ba798772e2f87f1d4d91dcde407a72e1c
Adrian <Adrian@192.168.1.39>
2017-03-23 22:50:31 +0100

    falta solo el fondo y random en columnas22

--- src/VentanaJuego.java
+++ src/VentanaJuego.java
@@ -65,9 +65,9 @@ public class VentanaJuego extends javax.swing.JFrame {
         // tamaño del jdialog con el mensaje al perder
         lose.setSize(ANCHOPANTALLA, ALTOPANTALLA);
         // indicamos el tamaño que va a tener el jDialog al presionar el boton escape 
-        fin.setSize(ANCHOPANTALLA/2,ALTOPANTALLA/2);
+        fin.setSize(ANCHOPANTALLA,ALTOPANTALLA);
         // y su posicion 
-        fin.setLocation(100, 20);
+        fin.setLocation(10, 20);
         // comienza el juego al presionar el boton.
         temporizador.start();
         for (int i=0; i<numColumnas; i++){
@@ -155,6 +155,7 @@ public class VentanaJuego extends javax.swing.JFrame {
         fin = new javax.swing.JDialog();
         continuar = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         lose = new javax.swing.JDialog();
         jLabel1 = new javax.swing.JLabel();
@@ -167,6 +168,7 @@ public class VentanaJuego extends javax.swing.JFrame {
         fin.setSize(new java.awt.Dimension(325, 200));
         fin.getContentPane().setLayout(null);
 
+        continuar.setFont(new java.awt.Font("Wide Latin", 0, 12)); // NOI18N
         continuar.setText("CONTINUAR");
         continuar.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -174,8 +176,9 @@ public class VentanaJuego extends javax.swing.JFrame {
             }
         });
         fin.getContentPane().add(continuar);
-        continuar.setBounds(20, 74, 130, 23);
+        continuar.setBounds(30, 300, 240, 50);
 
+        jButton1.setFont(new java.awt.Font("Wide Latin", 0, 12)); // NOI18N
         jButton1.setText("Nueva partida");
         jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -183,11 +186,18 @@ public class VentanaJuego extends javax.swing.JFrame {
             }
         });
         fin.getContentPane().add(jButton1);
-        jButton1.setBounds(20, 33, 130, 23);
+        jButton1.setBounds(40, 240, 200, 50);
+
+        jLabel5.setFont(new java.awt.Font("Trebuchet MS", 3, 14)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel5.setText("Donald trump debe identificar a los inmugrantes antes de la construccion de su muro");
+        fin.getContentPane().add(jLabel5);
+        jLabel5.setBounds(20, 40, 610, 100);
 
         jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/bandera.jpg"))); // NOI18N
         fin.getContentPane().add(jLabel4);
-        jLabel4.setBounds(0, 0, 300, 190);
+        jLabel4.setBounds(0, 0, 650, 400);
 
         lose.getContentPane().setLayout(null);
 
@@ -353,6 +363,7 @@ public class VentanaJuego extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JDialog lose;
     private javax.swing.JButton reiniciar;
